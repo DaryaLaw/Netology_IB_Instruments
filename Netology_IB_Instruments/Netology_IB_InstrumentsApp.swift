b17c0105f67bc3bf60403e7a15bd3f5c8cb4ed05
«Darya <dvgoleva@gmail.com>
2022-07-13 16:14:37 +0300

    Initial Commit

---
+++ Netology_IB_Instruments/Netology_IB_InstrumentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Netology_IB_InstrumentsApp.swift
+//  Netology_IB_Instruments
+//
+//  Created by Darya on 13.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct Netology_IB_InstrumentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
