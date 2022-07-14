416c684daf8cdd4e408a0c5157feeb72bdac046f
«Darya <dvgoleva@gmail.com>
2022-07-14 18:57:57 +0300

    The project has been developed. The profile is displayed on the screen.

---
+++ Netology_IB_Instruments/ProfileViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Darya on 14.07.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
